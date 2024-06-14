60b5e10fb67bacba279da33b0fe330ad5c62e247
ArtCosGRP <artcosgroup@gmail.com>
2024-06-14 17:56:04 +0500

    cobalt

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -16,24 +16,36 @@
             <div class="main-text-holder container">
                 <div class="main-text">
                     <h2>WHO ARE WE</h2>
-                    <p><span class="main-text-highlight">Artcos Co., LTD.</span> operates in the markets of digital, industrial and construction products. The main activity is the <span class="main-text-highlight">supply</span> of digital, industrial and construction equipment and materials, spare parts and special equipment for large, medium and small businesses. Despite the relatively short period of its existence, the company occupies a certain position in the market and contributes to the rapidly developing economy of the country.</p>
+                    <p><span class="main-text-highlight">Artcos Co., LTD.</span> AVL Networks Co., LTD operates in the markets of digital, <span class="main-text-highlight">industrial and construction</span> products. The main activity is the <span class="main-text-highlight">supply of mining equipment</span> (lifting and enrichment equipment, vertical sinking and near-shaft yard, mixing and charging machines of the HANFO-REPUMP series, spare parts), digital, industrial and construction equipment and materials, spare parts and special equipment for large, medium and small businesses. Despite the relatively short period of its existence, the company occupies a certain position in the market and contributes to the rapidly developing economy of the country.</p>
                 </div>
-                <div class="main-img temp">
-                    <img src="@/assets/images/server.webp" alt="">
+                <div class="main-img container temp">
+                    <img src="@/assets/images/newImg/1.jpg" alt="">
+                    <img src="@/assets/images/newImg/2.jpg" alt="">
                 </div>
                 <div class="main-text">
                     <h2>our associates</h2>
-                    <p>Our company works with countries such as: <span class="main-text-highlight">China, South Korea, Taiwan, Thailand, UAE, CIS, USA</span> and <span class="main-text-highlight">EU</span> member states.
-                    The company is not directly or indirectly associated with the countries included in the sanctions list (OFAC).
-                    Production and sale of digital and home appliances is a promising area of activity. Companies that are engaged in such business, note a very high level of competition in their field.</p>
+                    <p>Our company works with countries such as: <span class="main-text-highlight">China, South Korea, Taiwan, Thailand, UAE, CIS, USA</span> and <span class="main-text-highlight">EU</span> member states. 
+                        The company is not directly or indirectly associated with the countries included in the sanctions list (OFAC).
+                    </p>
                 </div>
-                <div class="main-img temp">
-                    <img src="@/assets/images/const.png" alt="">
+                <div class="main-img container temp">
+                    <img src="@/assets/images/newImg/3.jpg" alt="">
+                    <img src="@/assets/images/newImg/6.jpg" alt="">
                 </div>
                 <div class="main-text">
                     <h2>provided services</h2>
-                    <p>Our company provides a <span class="main-text-highlight">full range</span> of services for the transportation of any goods from <span class="main-text-highlight">anywhere</span> in the world by any type of transport. With all the quantitative breadth of transport services provided, the main function of the company's activities remains <span class="main-text-highlight">unchanged</span> – cargo delivery according to the most convenient and economical scheme for the cargo owner.
-                    Our company also provides services for <span class="main-text-highlight">finding the necessary products or partners</span>, installation, installation supervision and technical support of the supplied products.</p>
+                    <p>
+                        <span class="main-text-highlight">Production and sale</span> of digital and home appliances is a promising area of activity. Companies that are engaged in such business, note a very high level of competition in their field. 
+                        Personal computers, smartphones, tablet computers, laptops and other high-tech equipment are quite common, without which it is difficult to imagine the life of a modern person.<br>
+
+                        Our company provides a <span class="main-text-highlight">full range</span> of services for the transportation of any goods from anywhere in the world by any type of transport. With all the quantitative breadth of transport services provided, the main function of the company's activities remains unchanged – <span class="main-text-highlight">cargo delivery</span> according to the most convenient and economical scheme for the cargo owner. <br>
+
+                        Our company also provides services for finding the <span class="main-text-highlight">necessary products or partners</span>, installation, installation supervision and technical support of the supplied products.<br>  
+                    </p>
+                </div>
+                <div class="main-img container temp">
+                    <img src="@/assets/images/newImg/5.jpg" alt="">
+                    <img src="@/assets/images/newImg/4.png" alt="">
                 </div>
                 <div class="main-text">
                     <p>We are a young, but rapidly developing company that has partnered with <span class="main-text-highlight">major</span> manufacturers.
@@ -42,10 +54,10 @@
                 </div>
                 <div class="main-text-holder-div"><RouterLink to="/contact"><img src="@/assets/images/Send.svg" alt="">Get in touch</RouterLink></div>
             </div>
-            <div class="main-img container">
+            <!-- <div class="main-img container">
                 <img src="@/assets/images/aboutPhoto.webp" alt="">
                 <RouterLink to="/contact"><img src="@/assets/images/Send.svg" alt="">Get in touch</RouterLink>
-            </div>
+            </div> -->
     </main>
     <div class="header-line sec"></div>
 </template>
